9138bcd7f47303c07f9bc7553f663d109e020c28
klomucaj <klodimucaj8@gmail.com>
2023-05-04 18:22:21 +0200

    Creation of UsersServiceImpl class

---
+++ backend/src/main/java/it/unicam/smartparking/service/UsersServiceImpl.java
@@ -0,0 +1,31 @@
+package it.unicam.smartparking.service;
+
+import it.unicam.smartparking.dto.LoginUserDto;
+import it.unicam.smartparking.dto.UserDto;
+import it.unicam.smartparking.dto.UsersDto;
+import it.unicam.smartparking.model.Permissions;
+import it.unicam.smartparking.model.Roles;
+import it.unicam.smartparking.model.Users;
+import it.unicam.smartparking.repository.RolesRepository;
+import it.unicam.smartparking.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private RolesRepository rolesRepository;
+
+
+
+}
